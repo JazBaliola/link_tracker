7fc3b6fdfbe8ad8f7e30bf50a9d215b9b5927cae
Jaz <contact@jazcodeit.ca>
2025-01-26 17:48:32 -0700

    Created outline for Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,52 +6,59 @@ const Home = () => {
         <div className='Home'>
 
             <div className='Front'>
-                <div>
+
+                <div className='front-header'>
                     <h1>Link Tracker</h1>
-                    <p>Create and Track Links <br/>
-                        <b>Link Tracker</b> is an advanced URL tracker offering analytics for link traffic and visitor insights on your site. Use Link Tracker to detect your IP, locate IPs geographically, and track device locations. It also checks URLs for hidden redirects to ensure safety.
+                    <p>Simple, Fast, Reliable and Easy to use.<br/>
+                        <b>Link Tracker</b> is an advanced URL tracker offering analytics for link traffic and visitor insights on your links. Use Link Tracker to detect IP, locate IPs geographically, and track device locations. It also checks URLs for hidden redirects to ensure safety.
                     </p>
                 </div>
+
+                <div className='front-buttons'>
+                    <button className='btn btn-success' onClick={() => window.open("./Login", "_self")}>Login</button>
+                    <button className='btn btn-primary' onClick={() => window.open("./Register", "_self")}>Register</button>
+                </div>
+
             </div>
 
             <div className='Instruction'>
 
-                <div className='Instruction-text'>
+                <div className='instruction-text'>
                     <h1>Instruction</h1>
                     <p>
                         <b>Link Tracker</b> is a tool that enables you to effortlessly locate and monitor the IP address of any individual in three straightforward steps:
                         <br/><br/>
-                        1. Input any URL into the Grabify platform to condense and initiate tracking.
-                        2. Distribute the newly shortened link to another individual and prompt them to engage with it.
-                        3.Collect the IP address along with a comprehensive set of analytical data through the dedicated tracking page linked to your Grabify URL.
+                        1. Input any URL into the platform to generate new sharable link and initiate tracking.
+                        2. Distribute the newly generated link to another individual and prompt them to engage with it.
+                        3. Collect the IP address along with a comprehensive set of analytical data through the dedicated tracking page linked to your newly generated link.
                     </p>
                 </div>
 
-                <div className='Instruction-images'>
+                <div className='instruction-images'>
                     
                     <div className='card1'>
-                        <i>Icon</i>
-                        <h3>Header</h3>
-                        <p>Text</p>
+                        <i className="bi bi-link-45deg"></i>
+                        <h3>Generate link</h3>
+                        <p>Enter a URL that you want to track.</p>
                     </div>
 
                     <div className='card2'>
-                        <i>Icon</i>
-                        <h3>Header</h3>
-                        <p>Text</p>
+                        <i className='bi bi-share-fill'></i>
+                        <h3>Share link</h3>
+                        <p>Share the newly generated shareable link with another user.</p>
                     </div>
 
                     <div className='card3'>
-                        <i>Icon</i>
-                        <h3>Header</h3>
-                        <p>Text</p>
+                        <i className='bi bi-graph-up-arrow'></i>
+                        <h3>View analytics</h3>
+                        <p>Grab IP address and other analytic data after the user clicks on your generated sharable link.</p>
                     </div>
 
                 </div>
             </div>
 
             <div className='Footer'>
-
+                <p>Copyright &copy; <span>2025</span> <a href='https://jazcodeit.ca' target='_blank'>JazCodeIt</a>.</p>
             </div>
 
         </div>
