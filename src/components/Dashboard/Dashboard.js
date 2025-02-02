c479c78b52bdca1bb9c905fd46ec26fa75c5dd63
Jaz <contact@jazcodeit.ca>
2025-02-02 13:37:22 -0700

    Update blueprint skeleton of dashboard component

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import './Dashboard.css';
 
 const Dashboard = () => {
     return(
         <div className='Dashboard'>
-            <h1>Dashboard</h1>
+            
+            <div className='sidebar'>
+                <h1>sidebar</h1>
+            </div>
+
+            <div className='main'>
+
+                <div className='chart'>
+                    <h1>chart</h1>
+                </div>
+
+                <div className='accountInfo'>
+
+                    <div className='limit'>
+                        <h1>limit</h1>
+                    </div>
+
+                    <div className='total'>
+                        <h1>total</h1>
+                    </div>
+
+                </div>
+                
+            </div>
+
         </div>
     );
 }
